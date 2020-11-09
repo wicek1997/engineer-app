ef55b683d1a92d02de3c39fdd23004962242be52
xashotx <57191314+xashotx@users.noreply.github.com>
2020-11-09 19:45:41 +0100

    1

--- src/pages/Home.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export function Home()
-{
-    return <h2>Tu sie wrzuci krotki opis do czego to sluzy.</h2>
-
-}
--- src/pages/SignIn.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export function SignIn()
-{
-    return <h2>Logowanie do strony kurde faja</h2>
-}
