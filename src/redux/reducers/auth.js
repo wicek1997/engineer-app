6222de80e6325501cbf3924bf588030cd79af034
xashotx <57191314+xashotx@users.noreply.github.com>
2021-01-21 22:14:16 +0100

    1.1.1

--- src/redux/reducers/auth.js
+++ src/redux/reducers/auth.js
@@ -1,9 +1,13 @@
+localStorage.setItem('access_token');
+
 const jwt = JSON.parse(localStorage.getItem('access_token'));
 
 let initialState = {};
 
 if(jwt) {
     const jwtPayload = '';
+
+    jwtPayload = parseJwt(jwtPayload);
 
     initialState = {
         isAuthenticated = true,
@@ -21,6 +25,20 @@ if(jwt) {
 }
 
 
+function parseJwt (token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+};
+
+
 export const authReducer = (state = initialState, {type, payload}) => {
     //cos
 }
+
+
+
